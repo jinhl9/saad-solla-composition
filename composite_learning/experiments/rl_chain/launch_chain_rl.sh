4006bdd827fb6635895db8e2f971134297ada7c6
jinhl9 <haya.jinlee@gmail.com>
2024-01-02 12:03:43 +0000

    Remove old files

--- composite_learning/experiments/rl_chain/launch_chain_rl.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "Launch chained RL vs. baseline exp"
-step=${@:$#}
-echo "${step}"
-for var in "${@:1:$#-1}"; do
-echo "Experiment for sequence length ${var}"
-python experiments/rl_chain/run_rl.py --update-frequency 200 --seq-length ${var} --logdir logs/len_${var}/ --lr-positive 10 --train-step ${step}
-done
